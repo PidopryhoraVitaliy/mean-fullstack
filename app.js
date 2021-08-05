301aca958c0bf1be7b32de224e85fc098d49a33b
PidopryhoraVitaliy <vpidopryhora@gmail.com>
2021-08-05 20:25:23 +0300

    32. Функционал заказов

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const positionRoutes = require('./routes/position')
 const keys = require('./config/keys')
 const app = express()
 
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(keys.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
     .then(() => console.log('* MongoDB connected.'))
     .catch(error => console.log(error))
 
